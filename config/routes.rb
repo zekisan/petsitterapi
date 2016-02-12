993648aa7d6caa0cc21cdf6edff0ea03e10370ec
Ezequiel <zeki-san@hotmail.com>
2016-02-12 00:17:41 -0200

    implementing search action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   scope '/api' do
     scope '/v1' do
-      resources :sitters
+      resources :sitters do
+        collection do
+          get 'search'
+        end
+      end
     end
   end
 end
