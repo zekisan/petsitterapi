bd1560d628ca4a08d9a6dfa22940c0e9fee0581b
Ezequiel <ezequiel.oliveira@iwaysoft.com.br>
2016-02-15 10:32:54 -0200

    Removed Geokit from distance calculation and added new fields to pet_owner table.

--- app/controllers/pet_owners_controller.rb
+++ app/controllers/pet_owners_controller.rb
@@ -2,7 +2,7 @@ class PetOwnersController < ApplicationController
 
   def search_sitters
     pet_owner = PetOwner.find params[:id]
-    sitters = pet_owner.search_near_sitters(pet_owner, params[:animals])
+    sitters = pet_owner.search_near_sitters(params[:animals])
     render json: sitters.as_json
   end
 end
--- app/models/concerns/distance_calculation.rb
+++ app/models/concerns/distance_calculation.rb
@@ -2,22 +2,42 @@ module Concerns
   module DistanceCalculation
     extend ActiveSupport::Concern
 
-    def search_near_sitters(pet_owner, animals)
+    def search_near_sitters(animals)
       sitters_for_animals = get_sitters_by_animals(animals)
-      current_owner_distance = Geokit::Geocoders::GoogleGeocoder.geocode pet_owner.address
       sitters_for_animals.all.map do |s|
-        s if (sitter_geocoder(s).distance_to current_owner_distance, units: :kms) <= 5
+        s if distance_between_points(self.latitude, self.longitude, s.latitude, s.longitude) <= 5
       end.compact
-    end
-
-    def sitter_geocoder(sitter)
-      @geocoder ||= Geokit::Geocoders::GoogleGeocoder.geocode sitter.address
     end
 
     def get_sitters_by_animals(animals)
       sitters_for_animals = Sitter.all
       sitters_for_animals = sitters_for_animals.joins(:animals).where(animals: {name: animals}) if animals
       sitters_for_animals
+    end
+
+    private
+
+    def distance_between_points(lat1, long1, lat2, long2)
+      dtor = Math::PI/180
+      r = 6378.14
+
+      rlat1 = lat1 * dtor
+      rlong1 = long1 * dtor
+      rlat2 = lat2 * dtor
+      rlong2 = long2 * dtor
+
+      dlon = rlong1 - rlong2
+      dlat = rlat1 - rlat2
+
+      a = power(Math::sin(dlat/2), 2) + Math::cos(rlat1) * Math::cos(rlat2) * power(Math::sin(dlon/2), 2)
+      c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+      d = r * c
+
+      return d
+    end
+
+    def power(num, pow)
+      num ** pow
     end
   end
 end
