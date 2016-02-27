df988729db214213cf89c0aee8d7996daa11644a
Ezequiel <zeki-san@hotmail.com>
2016-02-27 11:37:47 -0300

    Action for request_contact.

--- app/controllers/pet_owners_controller.rb
+++ app/controllers/pet_owners_controller.rb
@@ -1,12 +1,25 @@
 class PetOwnersController < ApplicationController
 
+  before_filter :get_petowner
+
   def search_sitters
-    pet_owner = PetOwner.find params[:id]
-    sitters = pet_owner.search_near_sitters(params[:animals])
+    sitters = @pet_owner.search_near_sitters(params[:animals])
     render json: sitters.as_json
   end
 
   def request_contact
+    @pet_owner.contacts.build(sitter_id: params[:sitter_id])
 
+    if @pet_owner.save
+      render json: 'ok'
+    else
+      render json: @pet_owner.errors.messages
+    end
+  end
+
+  private
+
+  def get_petowner
+    @pet_owner = PetOwner.find params[:id]
   end
 end
