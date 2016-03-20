2353283e78b5b1a71ef18e40280977a786ecafe5
Ezequiel <zeki-san@hotmail.com>
2016-03-20 18:34:20 -0300

    Vai.

--- app/controllers/sitters_controller.rb
+++ app/controllers/sitters_controller.rb
@@ -2,7 +2,7 @@ class SittersController < ApplicationController
   
   def index
     sitters = Sitter.all
-    render json: sitters.as_json(include: [{animals: { only: :name }}, {cares: { only: :name }}])
+    render json: sitters.as_json(include: [:animals, {cares: { only: :name }}])
   end
 
   def contacts
