72e94b7b4acd60edfc0bae33a7358138575acaa7
Ezequiel <zeki-san@hotmail.com>
2016-03-21 23:27:21 -0300

    Added total_value to contact.

--- db/migrate/20160322021336_add_total_value_to_contacts.rb
+++ db/migrate/20160322021336_add_total_value_to_contacts.rb
@@ -1,5 +1,5 @@
 class AddTotalValueToContacts < ActiveRecord::Migration
   def change
-    add_column :contacts, :total_value, :decimal, precision: 2, scale: 4
+    add_column :contacts, :total_value, :decimal, precision: 5, scale: 2
   end
 end
