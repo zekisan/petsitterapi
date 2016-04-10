e45bd7f3756dda930ebf8febd4e45bc26539df94
Ezequiel <zeki-san@hotmail.com>
2016-04-10 19:17:43 -0300

    Updates.

--- db/migrate/20160314235718_add_district_to_pet_owner.rb
+++ db/migrate/20160314235718_add_district_to_pet_owner.rb
@@ -1,5 +1,6 @@
 class AddDistrictToPetOwner < ActiveRecord::Migration
   def change
     add_column :pet_owners, :district, :string
+    add_column :pet_owners, :photo, :string
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ Sitter.create([{name: 'Letícia', address: 'Rua General Lima e Silva, 500 - Cent
                 about_me: 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.',
                 latitude: -30.0376441, longitude: -51.2222909, photo: 'sitter1', header_background: 'header_background_1',
                 animals: [animals[1]], cares: [cares[0], cares[1]]},
-               {name: 'Ezequiel', address: 'R. Cel. Genuino, 149 - Centro Histórico, Porto Alegre - Rio Grande do Sul',
+               {name: 'Márcio Lopes', address: 'R. Cel. Genuino, 149 - Centro Histórico, Porto Alegre - Rio Grande do Sul',
                 district: 'Centro Histórico', value_hour: 15.0, value_shift: 60.0, value_day: 175.0,
                 about_me: 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.',
                 latitude: -30.035148, longitude: -51.2267789, photo: 'sitter2', header_background: 'header_background_2',
@@ -34,4 +34,4 @@ Sitter.create([{name: 'Letícia', address: 'Rua General Lima e Silva, 500 - Cent
                 latitude: -30.0375021, longitude: -51.22195079999999, photo: 'sitter4', header_background: 'header_background_4',
                 animals: [animals[1]], cares: [cares[0], cares[1]]}])
 
-PetOwner.create(name: 'Ezequiel Guilherme', district: 'Centro Histórico', address: 'R. Cel. Genuino, 149 - Centro Histórico, Porto Alegre - Rio Grande do Sul', latitude: -30.035148, longitude: -51.2267789)
+PetOwner.create(name: 'Ezequiel Guilherme', district: 'Centro Histórico', address: 'R. Cel. Genuino, 149 - Centro Histórico, Porto Alegre - Rio Grande do Sul', latitude: -30.035148, longitude: -51.2267789, photo: 'me')
