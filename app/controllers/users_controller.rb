ab26bec9df036b44a3387fdbd41aa7b6af8009af
Ezequiel <zeki-san@hotmail.com>
2016-04-30 12:15:58 -0300

    vai

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def insert_photo
     photo = Photo.create(image: params[:image])
-    user = User.find(params[:user_id])
+    user = User.find(params[:id])
     user.photo = photo
     user.save
   end
