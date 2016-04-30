a63f2270a110f8ad01b31e691c547e1cf729a4f6
Ezequiel <zeki-san@hotmail.com>
2016-04-30 13:51:16 -0300

    vai

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider => 'AWS',
-      :aws_access_key_id => 'AKIAJE4MFROVZYHWGSSQ',
-      :aws_secret_access_key => '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',
+      :aws_access_key_id => 'AKIAIFT25WAXDN6ZM36A',
+      :aws_secret_access_key => '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',
       :region => 'sa-east-1'
   }
   config.fog_directory = "testezekisan"
