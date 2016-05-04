0bd581688dd3c1d745b1b6f507e94e6de4a70930
Ezequiel <zeki-san@hotmail.com>
2016-05-03 22:11:38 -0300

    vai

--- app/controllers/sitters_controller.rb
+++ app/controllers/sitters_controller.rb
@@ -22,13 +22,23 @@ class SittersController < ApplicationController
 
   def contacts
     sitter = Sitter.find(params[:id])
-    render json: sitter.contacts.as_json(include: [:sitter, :pet_owner, :animals,
-                                                   rate: { include: [:sitter_comment, :pet_owner_comment]}])
+    render json: json_for_contacts(sitter.contacts)
   end
 
   def show
     sitter = Sitter.find params[:id]
     render json: sitter.sitter_json
+  end
+
+  private
+
+  def json_for_contacts(contacts)
+    contacts.map do |c|
+      { id: c.id, app_id: c.app_id, date_start: c.date_start, date_final: c.date_final,
+        time_start: c.time_start, time_final: c.time_final, created_at: c.created_at,
+        status_cd: c.status_cd, total_value: c.total_value, owner: c.pet_owner.owner_json,
+        sitter: c.sitter.sitter_json, animals: c.animals.map { |a| {id: a.id, name: a.name} } }
+    end
   end
 
 end
