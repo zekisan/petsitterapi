98c5316b1f0964ea993e9cd872c723c01f4b400f
Ezequiel <zeki-san@hotmail.com>
2016-05-25 21:50:15 -0300

    vai

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,7 +1,7 @@
 class SummariesController < ApplicationController
 
   def insert_photo
-    photo = Photo.create(app_id: params[:photo_app_id], image: [:image])
+    photo = Photo.create(app_id: params[:photo_app_id], image: params[:image])
     Summary.find_by_app_id(params[:app_id]).update(photo: photo)
     render json: 'ok'
   end
