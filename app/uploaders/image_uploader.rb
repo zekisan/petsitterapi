414e659629276a29cf503dd592f298b9c0881e6c
Ezequiel <zeki-san@hotmail.com>
2016-05-29 14:52:57 -0300

    vai

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,6 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+=begin
   version :large do
     process resize_to_limit: [800, 800]
   end
@@ -19,5 +20,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   version :thumb, from_version: :medium do
     process resize_to_limit: [50, 50]
   end
+=end
 
 end
